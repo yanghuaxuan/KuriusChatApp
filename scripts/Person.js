13c78ee08f5d8ec37c44878c9f2e5387a95fd5db
veryverypog <yanghuaxuan@gmail.com>
2021-06-21 03:41:38 +0000

    exampleContacts works again

--- scripts/Person.js
+++ scripts/Person.js
@@ -1,5 +1,18 @@
+'use strict';
+
 export class Person {
+  name = "default name";
+  messages = [];
+  
   constructor(name) {
     this.name = name;
+  }
+
+  get name() {
+    return this.name;
+  }
+  
+  addMessage(message) {
+    this.messages.push(message);
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
+'use strict';
 import {Person} from './Person.js'
 
 export function main() {
-  let exampleContacts = ["Joe Mama", "Boss", "Chad"];
-  exampleContacts.forEach((value) => {
+  const JoeMama = new Person("Joe Mama");
+  const Bob = new Person("Bob the Builder");
+  const Boss = new Person("Boss");
+  let exampleContacts = [JoeMama, Bob, Boss];
+  exampleContacts.forEach((contact) => {
     // Create name for each contact
     let contactsElement = document.getElementById("contacts");
     let element = document.createElement("h5");
     element.id = "contacts-name";
-    let contactName = document.createTextNode(value);
+    let contactName = document.createTextNode(contact.name);
     element.appendChild(contactName);
     contactsElement.appendChild(element);
   })
