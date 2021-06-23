cdc91f58857d19ed42a52953885a755f07bbb4c6
3Gigs <yanghuaxuan@gmail.com>
2021-06-22 19:17:12 -0700

    Formatting

--- app/site/scripts/chat.js
+++ app/site/scripts/chat.js
@@ -1,31 +1,31 @@
 export function createChat(contacts) {
   attachContactsListener(contacts);
-} 
+}
 
 // Trying event delegation
-let attachContactsListener = function(contacts) {
+let attachContactsListener = function (contacts) {
   let contacts_container = document.getElementById("contacts-container");
-  contacts_container.onclick = function(event) {
+  contacts_container.onclick = function (event) {
     let target = event.target;
     changeChat(target, contacts);
   }
 }
 
-let changeChat = function(target, contacts) {
+let changeChat = function (target, contacts) {
   changeChatHeader(target);
   changeChatBox(target, contacts);
 }
 
 
-let changeChatHeader = function(target) {
+let changeChatHeader = function (target) {
   let chatHeader = document.getElementById("chat-header")
   chatHeader.innerHTML = target.id;
 }
 
-let changeChatBox = function(target, contacts) {
+let changeChatBox = function (target, contacts) {
   let chatBox = document.getElementById("chat-box");
-  if(chatBox.hasChildNodes()) {
-    while(chatBox.firstChild) {
+  if (chatBox.hasChildNodes()) {
+    while (chatBox.firstChild) {
       chatBox.removeChild(chatBox.firstChild);
     }
   }
